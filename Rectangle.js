4907ca889879af34edb490cce6a0f5886191f2fa
Minh <minhdotcom@gmail.com>
2019-04-26 10:49:34 +0700

    done

---
+++ Rectangle.js
@@ -0,0 +1,15 @@
+function Rectangle (x, y, width, height, color) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+
+    this.render = function () {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.fill();
+        ctx.closePath();
+    }
+}
---
+++ main.js
@@ -0,0 +1,4 @@
+var ctx = document.getElementById("canvas").getContext("2d");
+var rect1 = new Rectangle (10, 10, 200, 100, "#000000");
+
+rect1.render();
